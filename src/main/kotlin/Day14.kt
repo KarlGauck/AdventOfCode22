7652a14fe2c2bfa96d1ce2755f384502a252da31
Karl Gauck <kgauck@gmx.de>
2022-12-14 07:24:09 +0100

    tiny changes to input fun day 14

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -3,12 +3,12 @@ import kotlin.math.abs
 object Day14 {
 
     fun input(src: String): MutableList<Point> {
-        val lines = Utils.getLines(src).map { it.split(" -> ").map { it.split(",") } }
+        val lines = Utils.getLines(src).map { it.split(" -> ").map { Point(it.split(",")[0].toInt(), it.split(",")[1].toInt()) } }
         val points = mutableListOf<Point>()
         for (line in lines) {
             for (pI in 1 until line.size) {
-                var p0 = Point(line[pI-1][0].toInt(), line[pI-1][1].toInt())
-                val p1 = Point(line[pI][0].toInt(), line[pI][1].toInt())
+                var p0 = line[pI-1]
+                val p1 = line[pI]
                 val dir = (p1-p0)/ abs(p1.x-p0.x + p1.y-p0.y)
                 points.add(p0.clone())
                 while (p0 != p1) {
@@ -21,7 +21,7 @@ object Day14 {
     }
 
     fun part1() {
-        val points = input("14")
+        val points = input("14t")
         var grounded = true
         var sum = 0
         while (grounded) {
@@ -48,10 +48,9 @@ object Day14 {
     }
 
     fun part2() {
-        val points = input("14")
+        val points = input("14t")
         var sum = 1
         val floorY = points.maxOf { it.y } + 2
-        println("${points.minOf { it.x }} ${points.maxOf { it.x }} ${points.minOf { it.y }} ${points.maxOf { it.y }}")
         var sand = mutableSetOf(Point(500, 0))
         while(sand.isNotEmpty()) {
             val newSand = mutableSetOf<Point>()
